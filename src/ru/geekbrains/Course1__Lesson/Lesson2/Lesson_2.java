7ae420dc5274ba81c3060b14acddef9a3615076d
Kotuma <aikotuma@gmail.com>
2019-03-05 00:11:45 +0400

    Lesson_2 PrintOut

--- src/ru/geekbrains/Course1__Lesson/Lesson2/Lesson_2.java
+++ src/ru/geekbrains/Course1__Lesson/Lesson2/Lesson_2.java
@@ -3,5 +3,6 @@ package ru.geekbrains.Course1__Lesson.Lesson2;
 public class Lesson_2 {
     public static void main(String[] args) {
         System.out.println("Результат расчёта выражения: ");
+        System.out.println("Lesson2 ");
     }
 }
