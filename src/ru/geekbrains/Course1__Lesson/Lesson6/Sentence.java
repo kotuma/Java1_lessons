70a932b68b0f60a0709b2ea47494c5aaebdc5c54
Kotuma Alexander <aikotuma@gmail.com>
2019-03-18 01:10:17 +0400

    Lesson_6. Text corrector - final

--- src/ru/geekbrains/Course1__Lesson/Lesson6/Sentence.java
+++ src/ru/geekbrains/Course1__Lesson/Lesson6/Sentence.java
@@ -4,18 +4,18 @@ public class Sentence {
 
 
     public static void main(String[] args) {
-        String s = "Предложение  один     Теперь предложение    два   Предложение   три";
+        String s = "Предложение  один     Теперь предложение    два   Предложение   три  ";
         String s1 = s.replaceAll(" +", " ");
         StringBuilder s2 = new StringBuilder("");
 
-        for (int i = 0; i < s1.length(); i++) {
+        for (int i = 0; i < s1.length()-1; i++) {
 
-            if (s1.charAt(i) == ' ' && s1.charAt(i+1) >= 'А' && s1.charAt(i+1) <= 'Я') {
+            if (i > 0 && s1.charAt(i) == ' ' && s1.charAt(i+1) >= 'А' && s1.charAt(i+1) <= 'Я') {
                 s2.append('.');
                 s2.append(s1.charAt(i));
             } else {
                 s2.append(s1.charAt(i));
-                if (i == s1.length() - 1 && s1.charAt(i) != '.') {
+                if (i == s1.length() - 2 && s1.charAt(i) != '.') {
                     s2.append('.');
                 }
             }
